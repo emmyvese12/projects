c7e8c3e3231ee1ace9f0aedbd48e9709b33493fe
Emmy Veselinov <emmyveselinov@gmail.com>
2022-01-31 23:54:11 -0500

    added dubya translation list

---
+++ project-translation/dubya.dat.py
@@ -0,0 +1,10 @@
+unjustified:pre-emptive
+budget:lots of numbers
+kill:oblitifry
+destroyed:destructifried
+policy:policy stuff
+diplomacy:a preemptive military strike
+are:is
+internet:dark dungeons of the Innurnet
+
+
